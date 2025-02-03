36842e1330286be6c73df3fd5b0333d203690bfa
Donna-Marie Smith <donna.smith@wearecortex.com>
2025-02-03 15:57:58 +0000

    Testing Create Latest

--- createLatest.ps1
+++ createLatest.ps1
@@ -1,3 +1,17 @@
-Remove-item -ItemType Directory -Path "content/en/docs/latest/" -Force
-New-Item -ItemType Directory -Path "content/en/docs/latest/" -Force
-Copy-Item -Path "content/en/docs/2024.11/*" -Destination "content/en/docs/latest/" -Recurse -Force
+$Path = ".\config\_default\config.toml"
+$latestName = "latest"
+
+$LatestVersion = (Select-String -Path $Path -Pattern "latest = true" -Context 3, 0) -split "`"" 
+
+#Use $LatestVersion[1] as we know that the version is the first line returned and splitting on " makes it the 2nd index
+$Version = $LatestVersion[1]
+
+if (Test-Path ".\content\en\docs\$latestName"){
+    Remove-Item -LiteralPath ".\content\en\docs\$latestName" -Force -Recurse
+}
+New-Item -ItemType Directory -Path ".\content\en\docs\$latestName\" -Force
+Copy-Item -Path ".\content\en\docs\$Version\*" -Destination ".\content\en\docs\$latestName\" -Recurse -Force
+(Get-Content .\content\en\docs\latest\_index.md) -replace $Version, "Latest" | Set-Content .\content\en\docs\latest\_index.md
+
+New-Item -ItemType Directory -Path ".\content\static\$latestName\" -Force
+Copy-Item -Path ".\content\static\$Version\*" -Destination ".\content\static\$latestName\" -Recurse -Force
