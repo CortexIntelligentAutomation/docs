a103d9fa5b3db90e595472da3b60c705111b38fc
cortex-lp <86599307+cortex-lp@users.noreply.github.com>
2025-02-11 15:41:08 +0000

    Improve search speed and prevent page freeze

--- assets/js/offline-search.js
+++ assets/js/offline-search.js
@@ -1,14 +1,16 @@
-// Adapted from code by Matt Walters https://www.mattwalters.net/posts/hugo-and-lunr/
+
+/**
+ * This script is used for the offline search feature.
+ * It calls the worker.js to generate the index and search the index.
+ * 
+ * Adapted from code by Matt Walters https://www.mattwalters.net/posts/hugo-and-lunr/
+ */
 
 (function ($) {
     'use strict';
 
     $(document).ready(function () {
         const $searchInput = $('.td-search-input');
-
-        //
-        // Options for popover
-        //
 
         $searchInput.data('html', true);
         $searchInput.data('placement', 'bottom');
@@ -17,10 +19,127 @@
             '<div class="popover offline-search-result" role="tooltip"><div class="arrow"></div><h3 class="popover-header"></h3><div class="popover-body"></div></div>'
         );
 
-        //
-        // Register handler
-        //
+        let worker = null;
 
+        if (window.Worker) {
+            worker = new Worker('/js/worker.js');
+            const url = '/lunr-index.json';
+
+            worker.postMessage({
+                type: 'init',
+                lunrIndexUrl: url,
+                rawIndexUrl: $searchInput.data('offline-search-index-json-src')
+            });
+
+            worker.onerror = function (error) {
+                console.error('Error in worker:', error);
+            };
+        }
+
+        let currentTarget = null;
+
+        worker.onmessage = function (event) {
+            if (event.data.type === 'search') {
+                const docs = event.data.docs;
+                const $html = $('<div>');
+
+                $html.append(
+                    $('<div>')
+                        .css({
+                            display: 'flex',
+                            justifyContent: 'space-between',
+                            marginBottom: '1em',
+                        })
+                        .append(
+                            $('<span>')
+                                .text('Search results')
+                                .css({ fontWeight: 'bold' })
+                        )
+                        .append(
+                            $('<i>')
+                                .addClass('fas fa-times search-result-close-button')
+                                .css({
+                                    cursor: 'pointer',
+                                })
+                        )
+                );
+
+                const $searchResultBody = $('<div>').css({
+                    maxHeight: `calc(100vh - ${currentTarget.offset().top -
+                        $(window).scrollTop() +
+                        180
+                        }px)`,
+                    overflowY: 'auto',
+                });
+                $html.append($searchResultBody);
+
+                if (docs === undefined || docs.size === 0) {
+                    $searchResultBody.append(
+                        $('<p>').text(`No results found for query "${event.data.query}"`)
+                    );
+                } else {
+                    docs.forEach((doc, key) => {
+                        if (doc === undefined) {
+                            return;
+                        }
+
+                        const href =
+                            $searchInput.data('offline-search-base-href') +
+                            key.replace(/^\//, '');
+
+                        const $entry = $('<div>').addClass('mt-4').addClass('search-result');
+
+                        $entry.append(
+                            $('<a>')
+                                .addClass('d-block')
+                                .css({
+                                    fontSize: '1.2rem',
+                                })
+                                .attr('href', href)
+                                .text(doc.title)
+                        );
+
+                        $entry.append(
+                            $('<small>').addClass('d-block text-muted').text(key)
+                        );
+
+                        $entry.append($('<p>').text(doc.excerpt));
+
+                        $searchResultBody.append($entry);
+                    });
+                }
+
+                currentTarget.on('shown.bs.popover', () => {
+                    $('.search-result-close-button').on('click', () => {
+                        currentTarget.val('');
+                        currentTarget.trigger('change');
+                    });
+                });
+
+                currentTarget
+                    .data('content', $html[0].outerHTML)
+                    .popover('show');
+            }
+        }
+
+        const render = ($targetSearchInput) => {
+            $targetSearchInput.popover('dispose');
+            currentTarget = $targetSearchInput;
+
+            const searchQuery = $targetSearchInput.val();
+            if (searchQuery === '') {
+                return;
+            }
+
+            worker.postMessage({
+                type: 'search',
+                currentPath: window.location.pathname,
+                query: searchQuery,
+                maxResults: $targetSearchInput.data('offline-search-max-results')
+            });
+        };
+
+        // Renders the search results when the input changes.
         $searchInput.on('change', (event) => {
             render($(event.target));
 
@@ -32,177 +151,5 @@
         $searchInput.closest('form').on('submit', () => {
             return false;
         });
-
-        //
-        // Lunr
-        //
-
-        let idx = null; // Lunr index
-        const resultDetails = new Map(); // Will hold the data for the search results (titles and summaries)
-
-        // Set up for an Ajax call to request the JSON data file that is created by Hugo's build process
-        $.ajax($searchInput.data('offline-search-index-json-src')).then(
-            (data) => {
-                idx = lunr(function () {
-                    this.ref('ref');
-
-                    // If you added more searchable fields to the search index, list them here.
-                    // Here you can specify searchable fields to the search index - e.g. individual toxonomies for you project
-                    // With "boost" you can add weighting for specific (default weighting without boost: 1)
-                    this.field('title', { boost: 5 });
-                    this.field('categories', { boost: 3 });
-                    this.field('tags', { boost: 3 });
-                    this.field('description', { boost: 2 });
-                    this.field('body');
-
-                    const searchPath = $searchInput.data('search-path');
-
-                    data.forEach((doc) => {
-                        let docToAdd;
-                        if (searchPath !== undefined && doc.ref.startsWith(searchPath)) {
-                            docToAdd = doc;
-                        } else if (searchPath === undefined) {
-                            docToAdd = doc;
-                        }
-
-                        if (docToAdd
-                            && docToAdd.ref !== undefined
-                            && !docToAdd.ref.includes('/_shared/')
-                        ) {
-                            this.add(doc);
-
-                            resultDetails.set(doc.ref, {
-                                title: doc.title,
-                                excerpt: doc.excerpt,
-                            });
-                        }
-                    });
-                });
-
-                $searchInput.trigger('change');
-            }
-        );
-
-        const render = ($targetSearchInput) => {
-            // Dispose the previous result
-            $targetSearchInput.popover('dispose');
-
-            //
-            // Search
-            //
-
-            if (idx === null) {
-                return;
-            }
-
-            const searchQuery = $targetSearchInput.val();
-            if (searchQuery === '') {
-                return;
-            }
-
-            const results = idx
-                .query((q) => {
-                    const tokens = lunr.tokenizer(searchQuery.toLowerCase());
-                    tokens.forEach((token) => {
-                        const queryString = token.toString();
-                        q.term(queryString, {
-                            boost: 100,
-                        });
-                        q.term(queryString, {
-                            wildcard:
-                                lunr.Query.wildcard.LEADING |
-                                lunr.Query.wildcard.TRAILING,
-                            boost: 10,
-                        });
-                        q.term(queryString, {
-                            editDistance: 2,
-                        });
-                    });
-                })
-                .slice(
-                    0,
-                    $targetSearchInput.data('offline-search-max-results')
-                );
-
-            //
-            // Make result html
-            //
-
-            const $html = $('<div>');
-
-            $html.append(
-                $('<div>')
-                    .css({
-                        display: 'flex',
-                        justifyContent: 'space-between',
-                        marginBottom: '1em',
-                    })
-                    .append(
-                        $('<span>')
-                            .text('Search results')
-                            .css({ fontWeight: 'bold' })
-                    )
-                    .append(
-                        $('<i>')
-                            .addClass('fas fa-times search-result-close-button')
-                            .css({
-                                cursor: 'pointer',
-                            })
-                    )
-            );
-
-            const $searchResultBody = $('<div>').css({
-                maxHeight: `calc(100vh - ${$targetSearchInput.offset().top -
-                    $(window).scrollTop() +
-                    180
-                    }px)`,
-                overflowY: 'auto',
-            });
-            $html.append($searchResultBody);
-
-            if (results.length === 0) {
-                $searchResultBody.append(
-                    $('<p>').text(`No results found for query "${searchQuery}"`)
-                );
-            } else {
-                results.forEach((r) => {
-                    const doc = resultDetails.get(r.ref);
-                    const href =
-                        $searchInput.data('offline-search-base-href') +
-                        r.ref.replace(/^\//, '');
-
-                    const $entry = $('<div>').addClass('mt-4').addClass('search-result');
-
-                    $entry.append(
-                        $('<a>')
-                            .addClass('d-block')
-                            .css({
-                                fontSize: '1.2rem',
-                            })
-                            .attr('href', href)
-                            .text(doc.title)
-                    );
-
-                    $entry.append(
-                        $('<small>').addClass('d-block text-muted').text(r.ref)
-                    );
-
-                    $entry.append($('<p>').text(doc.excerpt));
-
-                    $searchResultBody.append($entry);
-                });
-            }
-
-            $targetSearchInput.on('shown.bs.popover', () => {
-                $('.search-result-close-button').on('click', () => {
-                    $targetSearchInput.val('');
-                    $targetSearchInput.trigger('change');
-                });
-            });
-
-            $targetSearchInput
-                .data('content', $html[0].outerHTML)
-                .popover('show');
-        };
     });
 })(jQuery);
